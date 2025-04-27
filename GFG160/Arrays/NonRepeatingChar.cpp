234b37fbacc326d66b1b6b7a413eba142041e8ad
Yashavanthh <g.h.yashavanth@gmail.com>
2025-04-27 22:44:24 +0530

    Add solution for Non repeating character

---
+++ GFG160/Arrays/NonRepeatingChar.cpp
@@ -0,0 +1,19 @@
+
+class Solution {
+    public:
+      char nonRepeatingChar(string &s) {
+          // Your code here
+          int freq[26] = {0};
+          
+          for(char c : s){
+              freq[c - 'a']++;
+          }
+          
+          for(char c : s){
+              if(freq[c - 'a'] == 1)
+                  return c;
+          }
+          return '$';
+      }
+  };
+  
