5bdbfbe9b6fa4cde2355e0854c2f6ca7d1e53189
Yashavanthh <g.h.yashavanth@gmail.com>
2025-05-04 07:47:34 +0530

    Add solution for overlapping intervals

---
+++ GFG160/Arrays/overlappingIntervals.cpp
@@ -0,0 +1,23 @@
+
+class Solution {
+    public:
+      vector<vector<int>> mergeOverlap(vector<vector<int>>& arr) {
+          // Code here
+          vector<vector<int>> merged;
+          if(arr.empty()) return merged;
+          
+          sort(arr.begin(), arr.end());
+          
+          merged.push_back(arr[0]);
+          
+          for(int i = 1; i < arr.size(); ++i){
+              vector<int>& last = merged.back();
+              if(arr[i][0] <= last[1]){
+                  last[1] = max(last[1], arr[i][1]);
+              }else{
+                  merged.push_back(arr[i]);
+              }
+          }
+          return merged;
+      }
+  };
